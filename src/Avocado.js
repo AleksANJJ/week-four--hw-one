d0b639137ffc8dac9b50c5f4419e609daec6c10c
aleksANJJ <meshkova8171@gmail.com>
2022-01-20 11:51:32 +0200

    first commit

---
+++ src/Avocado.js
@@ -0,0 +1,7 @@
+import imageTwo  from './avocado.jpeg';
+
+function Avocado() {
+    return <img src={imageTwo} width="650px"/>
+}
+
+export default Avocado;
---
+++ src/Tomato.js
@@ -0,0 +1,7 @@
+import imageOne  from './tomato.jpeg';
+
+function Tomato() {
+    return <img src={imageOne} width="650px"/>
+}
+
+export default Tomato;
